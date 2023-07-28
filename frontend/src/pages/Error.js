f7a774bad7e969f04da6a028652ae785b39855d2
Peto Gurin <no.mi83@yahoo.com.vn>
2023-07-28 16:08:32 +0700

    refactor throw error by throw json instread for less code

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -8,7 +8,7 @@ export default function Error() {
     let title = "hehe boids"
     let message = "Something go crazy"
     if(error.status === 500){
-        message = JSON.parse(error.data).message
+        message = error.data.message
     }
 
     if(error.status === 404){
