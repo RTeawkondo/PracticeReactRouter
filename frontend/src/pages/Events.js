0e8742a52b03b9f74b5e0430528862a096726922
Peto Gurin <no.mi83@yahoo.com.vn>
2023-07-30 15:23:21 +0700

    pre display page before data is loaded by using defer + useLoaderData + Await component + Suspense component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,22 +1,35 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events
-  return <EventsList events={events} />;
+  const { events } = useLoaderData();
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading....</p>}>
+      <Await resolve={events}>
+        {(loadedEvent) => <EventsList events={loadedEvent} />}
+      </Await>
+    </Suspense>
+  );
   //return <EventsList />;
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvent() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw json({message: "hehe boids"}, {status: 500})
+    throw json({ message: "hehe boids" }, { status: 500 });
   } else {
-    return response
+    const resData = await response.json()
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvent(),
+  });
 }
